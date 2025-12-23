bd85b119cba55deaba3cec5b6ec6219c685b17d4
Henning Sieh <admin@henningsieh.de>
2025-12-23 10:45:01 +0100

    feat: add favicon images and site manifest for improved branding

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -66,6 +66,24 @@ export default async function LocaleLayout({ children, params }: Props) {
       className={`${comfortaa.variable} ${jetbrainsMono.variable} ${sourceSerif4.variable} scroll-smooth`}
     >
       <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         {env.NODE_ENV === "development" && (
           <script
             async
